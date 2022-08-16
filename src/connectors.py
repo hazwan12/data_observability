d5dc5d1ef364d93ab32dfea3365355a21695193c
Hazwan <hazwan12@hotmail.com>
2022-08-16 23:38:10 +0800

    Initial Commit

---
+++ src/connectors.py
@@ -0,0 +1,14 @@
+class BaseConnector:
+
+    def __init__(self) -> None:
+        pass
+
+class ObjectStoreConnector(BaseConnector):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+class JDBCConnector(BaseConnector):
+
+    def __init__(self) -> None:
+        super().__init__()
